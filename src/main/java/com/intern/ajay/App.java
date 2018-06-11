602fd717d270d5f6bb3f90b339828e913f366481
ajay <ajay595uk@gmail.com>
2018-06-11 23:38:57 +0530

    first commit

---
+++ src/main/java/com/intern/ajay/App.java
@@ -0,0 +1,13 @@
+package com.intern.ajay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
